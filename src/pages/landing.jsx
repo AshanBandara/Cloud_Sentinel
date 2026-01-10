b2844b32c92974dcf8712b303cb422bf513e4b53
Ashan <ashanbandara407@gmail.com>
2026-01-10 14:52:56 +0530

    hero sec done

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,7 +1,33 @@
 import "../styles/landing.css";
 
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+    return (
+        <div>
+            <header> 
+                
+            </header>
+            <main>
+                <section className="hero">
+                    <div className="hero-overlay">
+                        <div className="hero-content">
+                            <h1>
+                                Serverless Security <br /> <span>Made Effortless</span>
+                            </h1>
+
+                            <p>
+                                Detect, monitor, and protect AWS Lambda functions from
+                                misconfigurations and security risks — in real time.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+                
+            </main>
+            <footer>
+                <p>© 2024 Cloud Sentinel. All rights reserved.</p>
+            </footer>
+        </div>
+    );
 };
 
 export default Landing;
