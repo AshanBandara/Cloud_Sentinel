cf7c99c8facf541e0277d2792aa38f7714e5f068
Raneesha Ranasinghe <raneesharanasinghe@gmail.com>
2026-01-11 01:46:13 +0530

    Add Cloud Sentinel header with new logo and styling. pushing it back again.

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,35 +1,62 @@
 import "../styles/landing.css";
 
 const Landing = () => {
-    return (
-        <div>
-            <header> 
+  return (
+    <div>
+      <header>
+        <nav className="navbar">
+          <div className="logo">
+            <div className="logo-icon">
+              <img src="src\assets\logo.png" alt="Cloud Sentinel Logo" />
+            </div>
+            <span className="logotext">Cloud Sentinel</span>
+          </div>
 
-            </header>
-            <main>
-                <section className="hero">
-                    <div className="hero-overlay">
-                        <div className="hero-content">
-                            <h1>
-                                Serverless Security <br /> <span>Made Effortless</span>
-                            </h1>
+          <ul className="nav-links">
+            <li>
+              <a href="#features">Features</a>
+            </li>
+            <li>
+              <a href="#pricing">Pricing</a>
+            </li>
+            <li>
+              <a href="#roadmap">Roadmap</a>
+            </li>
+            <li>
+              <a href="#docs">Docs</a>
+            </li>
+          </ul>
 
-                            <p>
-                                Detect, monitor, and protect AWS Lambda functions from
-                                misconfigurations and security risks — in real time.
-                            </p>
-                            
-                            <button className="hero-btn">Get Started</button>
-                        </div>
-                    </div>
-                </section>
-                
-            </main>
-            <footer>
-                <p>© 2024 Cloud Sentinel. All rights reserved.</p>
-            </footer>
-        </div>
-    );
+          <div className="nav-actions">
+            <button className="btn-login">Log in</button>
+            <button className="btn-primary">Start Free</button>
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        <section className="hero">
+          <div className="hero-overlay">
+            <div className="hero-content">
+              <h1>
+                Serverless Security <br /> <span>Made Effortless</span>
+              </h1>
+
+              <p>
+                Detect, monitor, and protect AWS Lambda functions from
+                misconfigurations and security risks — in real time.
+              </p>
+
+              <button className="hero-btn">Get Started</button>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <p>© 2024 Cloud Sentinel. All rights reserved.</p>
+      </footer>
+    </div>
+  );
 };
 
 export default Landing;
